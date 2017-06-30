0c7f47a56d51b3f46ab9b92acd4548b619f49643
LION <ksmart.lion@gmail.com>
2017-06-30 09:15:30 -0700

    create new  member

---
+++ app/Modules/Member/Providers/ModuleServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Modules\Member\Providers;
+
+use Caffeinated\Modules\Support\ServiceProvider;
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the module services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../Resources/Lang', 'member');
+        $this->loadViewsFrom(__DIR__.'/../Resources/Views', 'member');
+        $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations', 'member');
+    }
+
+    /**
+     * Register the module services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(RouteServiceProvider::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
 Route::get('/photo/{template}/{filename}','ImageCController@getResponse');
+
+Route::get('/n',function (){
+    $m = \App\User::get();
+    \Illuminate\Support\Facades\Notification::send($m,new \App\Notifications\PostNotification());
+});
